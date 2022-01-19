e9eae6e527f31a25dc96744ed3d9efc49f499ebf
eoin <eoinoreilly30@gmail.com>
2022-01-19 18:32:01 +0000

    f

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: config => {
+    config.output.filename = "js/[name].[contenthash].js";
+    config.output.chunkFilename = "js/[name].[contenthash].js";
+
+    config.plugins.push(new PrerenderSpaPlugin({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/'],
+      renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+        // We need to inject a value so we're able to
+        // detect if the page is currently pre-rendered.
+        inject: {},
+        headless: false
+      }),
+    }),);
+  },
 }
